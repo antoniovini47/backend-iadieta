c4e1527203eed15d540288d4c2d0957ca2457ddd
Antonio Oliveira <antoniovini47@gmail.com>
2025-03-11 12:23:46 -0300

    feat: create a get on root for status

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,14 @@ const server = fastify({ logger: true });
 
 server.register(geminiRoutes);
 
+server.get("/", async (request, reply) => {
+  return reply.send({ message: "API is running..." });
+});
+
 const start = async () => {
   try {
     await server.listen({ port: 3000 });
-    console.log("Server running at http://localhost:3000");
+    console.log(`Server running...`);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
